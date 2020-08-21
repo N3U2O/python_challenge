5b2364a7efdf7927feed29af79630c3fb08fe164
Adrian Gugyin <n3u2o@protonmail.com>
2020-08-21 18:50:52 +0200

    Create single.py
    whatever. u0

---
+++ single.py
@@ -0,0 +1,9 @@
+# I think I am obliged to do it now... :) Let's go to http://www.pythonchallenge.com/
+import this
+print("I know it's old... but time to really explore the 'P. C.' with Python 3 this time!")
+u0 = "http://www.pythonchallenge.com/pc/def/0.html"
+# some conventional BS: I'll call the challenge urls u#, where # is a number (single or multi-digit)
+print("Hm. Let's play, what is 2**38, py'?")
+print("[PYTRON+2@2@] -- Bzzzt. boop. thinking...")
+answer = 2 ** 38
+print(f"[...] Got it. It is {answer}")
